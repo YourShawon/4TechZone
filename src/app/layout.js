02537fe1fb9d13979e3d503ce2b50a017ae5e463
YourShawon <yourshawon101@gmail.com>
2025-05-14 12:45:23 +0600

    add paths file to store all the paths in one place

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${inter.variable} ${jakarta_sans.variable} font-jakarta-sans antialiased`}
+        className={`${inter.variable} ${jakarta_sans.variable} font-inter antialiased`}
       >
         {children}
       </body>
---
+++ src/app/paths.js
@@ -0,0 +1,5 @@
+export const homePath = () => "/";
+export const aboutPath = () => "/about";
+export const contactPath = () => "/contact";
+export const servicesPath = () => "/services";
+export const testimonialsPath = () => "/testimonials";
